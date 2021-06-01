4f31f0a1f1b0cf1aff8db36246b90a5b68f2275b
Leonardo Ayres <leoayresec@gmail.com>
2021-06-01 12:11:03 -0300

    Configuração das rotas

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+class Home extends React.Component {
+  render(){
+    return (
+      <Text>Home</Text>
+    )
+  }
+}
+
+export default Home;
---
+++ src/pages/User/Cadastro/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Cadastro = () => {
+  return <View />;
+}
+
+export default Cadastro;
