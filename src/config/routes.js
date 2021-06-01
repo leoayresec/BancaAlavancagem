6f96255c396b6716c73bb4d9661d2f2517d10dce
Leonardo Ayres <leoayresec@gmail.com>
2021-06-01 12:58:35 -0300

    Iniciando tela de login

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from '../pages/Home'
+import Home from '../pages/Home';
+import Login from '../pages/User/Login'
 
 const Stack = createStackNavigator();
 
 function Routes() {
   return (
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login}  options={{headerShown:false}}/>
       </Stack.Navigator>
   );
 }
--- src/pages/User/Login/index.js
+++ src/pages/User/Login/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import styles from './styles'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
 
-// import { Container } from './styles';
-
-const Login = () => {
-  return <View />;
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: '',
+      senha: ''
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Input
+          label='E-mail'
+          containerStyle={styles.input}
+          placeholder='bancaalavancagem@email.com'
+          leftIcon={
+            <Icon
+              name='user'
+              size={20}
+              color='black'
+            />
+          }
+        />
+        <Input
+          secureTextEntry={true}
+          label='Senha'
+          containerStyle={styles.input}
+          placeholder='*********'
+          leftIcon={
+            <Icon
+              name='unlock-alt'
+              size={20}
+              color='black'
+            />
+          }
+        />
+      </View>
+    )
+  }
 }
 
 export default Login;
